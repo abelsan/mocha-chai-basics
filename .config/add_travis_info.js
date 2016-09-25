030170e22dbe42bfccd5fc3fa4e0663964077e89
abelsan <abel@mit.edu>
2016-09-24 23:13:41 -0400

    fixed paths in add_travis_info.js

--- .config/add_travis_info.js
+++ .config/add_travis_info.js
@@ -1,15 +1,15 @@
 var fs = require('fs');
 
 // the test results
-var test = fs.readFileSync('test.json');
+var test = fs.readFileSync('.config/test.json');
 var test = JSON.parse(test);
 
 // the test results
-var travis = fs.readFileSync('../travis.json');
+var travis = fs.readFileSync('travis.json');
 var travis = JSON.parse(travis);
 
 //change the value in the in-memory object
 test.travis = travis;
 
 //Serialize as JSON and Write it to a file
-fs.writeFileSync('test.json', JSON.stringify(test));
+fs.writeFileSync('.config/test.json', JSON.stringify(test));

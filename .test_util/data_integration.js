6b16e9e21e63873a947f814aed6cea5f3621301a
abelsan <abel@mit.edu>
2016-09-25 00:28:20 -0400

    updated file names for readability

--- .test_util/data_integration.js
+++ .test_util/data_integration.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 // the test results
-var test = fs.readFileSync('.config/test.json');
+var test = fs.readFileSync('.test_util/test.json');
 var test = JSON.parse(test);
 
 // the test results
@@ -12,4 +12,4 @@ var travis = JSON.parse(travis);
 test.travis = travis;
 
 //Serialize as JSON and Write it to a file
-fs.writeFileSync('.config/combined.json', JSON.stringify(test));
+fs.writeFileSync('.test_util/data_integration.json', JSON.stringify(test));
